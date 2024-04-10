10c963b83b7f7e97f97de6ec68e21b1f8e18d033
Joshua <jhellst@gmail.com>
2024-04-09 19:39:03 -0700

    Added backtracking-style solution for tracking depth via parentheses.

---
+++ 22 - Generate Parentheses.py
@@ -0,0 +1,41 @@
+# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+# Example 1:
+
+# Input: n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+# Example 2:
+
+# Input: n = 1
+# Output: ["()"]
+
+# Constraints:
+
+# 1 <= n <= 8
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # Backtracking. Keep track of counts of open and closed parens.
+
+        # stack = []
+        res = []
+
+        def backtrack(openCount, closedCount, stack):
+            if openCount == closedCount == n: # We have used all parens.
+                res.append("".join(stack))
+                return
+
+            if openCount < n: # Open paren is an option.
+                stack.append("(")
+                backtrack(openCount + 1, closedCount, stack)
+                stack.pop()
+            if closedCount < openCount:
+                stack.append(")")
+                backtrack(openCount, closedCount + 1, stack)
+                stack.pop()
+
+
+        backtrack(0, 0, [])
+        return res
