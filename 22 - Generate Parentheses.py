700608e151288520619b9b8add86cde89969c7c0
Joshua <jhellst@gmail.com>
2024-08-19 16:33:36 -0700

    Added optimal recursive solution to generate parentheses, using a stack.

--- 22 - Generate Parentheses.py
+++ 22 - Generate Parentheses.py
@@ -43,3 +43,37 @@ class Solution:
 
 # Time Complexity: O(4^n / sqrt(n))
 # Space Complexity: O(n) -> Stack may contain up to n calls at a time.
+
+
+
+# 2nd Solution:
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # Use a stack, pop and append to simulate different combos.
+        # Each group of parens "()" must be nested or sequential
+        #   - Must start with open paren AND we can't ever have more closed than open on the stack.
+
+        res = []
+
+        def recurse(num_open, num_closed, stack):
+            if num_open == num_closed == n:
+                res.append("".join(stack))
+                return
+            if num_open < n:
+                stack.append("(")
+                recurse(num_open + 1, num_closed, stack)
+                stack.pop()
+            if num_closed < num_open:
+                stack.append(")")
+                recurse(num_open, num_closed + 1, stack)
+                stack.pop()
+            return
+
+        res = []
+
+        recurse(0, 0, [])
+        return res
+
+# Time Complexity: O(n^2) -> O(4^n / n*sqrt(n))
+# Space Complexity: O(n) -> Recursive calls stored on stack -> Maximum stack size will be 2*n.
