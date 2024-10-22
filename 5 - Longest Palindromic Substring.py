95caa85a81be7c536ca33999b39c3a1ca6b4400b
Joshua <jhellst@gmail.com>
2024-10-22 12:08:12 -0700

    Added solution to find longest palindromic substring.

--- 5 - Longest Palindromic Substring.py
+++ 5 - Longest Palindromic Substring.py
@@ -51,3 +51,40 @@ class Solution:
 
 # Time Complexity: O(n^2) -> Loop through each index, then expand outward (potentially to entire string).
 # Space ComplexityL O(1) -> No additional storage needed.
+
+
+
+# 2nd Solution:
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # Find the longest palindromic substring within s.
+        # DP-style solution. At each index, we'll store the max_substring_length (and the max_substring).
+        #   - Need to check for palindromes in strings of 1) even-length and 2) odd-length.
+
+        res, res_len = "", 0
+
+        # Odd-length -> check x units from i.
+        for i in range(len(s)):
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]: # Continue expanding as long as it remains a palindrome.
+                if (r - l + 1) >= res_len:
+                    res = s[l:r+1]
+                    res_len = r - l + 1
+                l -= 1
+                r += 1
+
+        # Even-length -> check 2 center units.
+        for i in range(len(s) - 1):
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]: # Continue expanding as long as it remains a palindrome.
+                if (r - l + 1) >= res_len:
+                    res = s[l:r+1]
+                    res_len = r - l + 1
+                l -= 1
+                r += 1
+
+        return res
+
+# Time Complexity: O(n) -> Visit every index of x up to 2 times.
+# Space Complexity: O(1) -> No additional storage used.
