ded89e02cf578d6f4838af16d8c592102c60a856
Joshua <jhellst@gmail.com>
2024-11-15 16:21:46 -0800

    Added iterative (deque) BFS-style solution to return level-order traversal of binary tree.

--- 102 - Binary Tree Level Order Traversal.py
+++ 102 - Binary Tree Level Order Traversal.py
@@ -53,3 +53,40 @@ class Solution:
 
 # Time Complexity: O(n) -> Visit every node in the tree exactly 1 time.
 # Space Complexity: O(w) -> O(n) -> Store up to w nodes on the stack, where w is the width of any given level of the tree.
+
+
+
+# 2nd Solution:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # Return the level-order traversal of the nodes values.
+        #   - BFS-style traversal (iterative using a deque). Keep track of the entire level, and append it.
+
+        res = []
+        q = deque()
+        q.append(root)
+
+        while q:
+            q_len = len(q)
+            cur_level = []
+
+            for i in range(q_len):
+                cur_node = q.popleft()
+                if cur_node:
+                    cur_level.append(cur_node.val)
+                    q.append(cur_node.left)
+                    q.append(cur_node.right)
+            if cur_level:
+                res.append(cur_level)
+
+        return res
+
+# Time Complexity: O(n) -> Visit every node in the binary tree once.
+# Space Complexity: O(w) -> O(n) -> Store nodes on deque up to size of current level of the tree. In worst case, w == n.
