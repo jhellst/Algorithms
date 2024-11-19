25b716475e3e4d397b1fdb690bb1cef33c2c96e5
Joshua <jhellst@gmail.com>
2024-11-19 10:58:09 -0800

    Added recursive (optimal) solution to determine if 2 binary trees are the same.

--- 100 - Same Tree.py
+++ 100 - Same Tree.py
@@ -45,3 +45,29 @@ class Solution:
 
 # Time Complexity: O(n) -> Must visit every node in each tree once.
 # Space Complexity: O(h) -> O(n) -> Store recursive calls on the stack, up to height of the tree.
+
+
+
+# 2nd Solution:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # Return True if the trees are identical, otherwise return False.
+
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        elif p.val != q.val:
+            return False
+        else:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+# Time Complexity: O(n) -> In worst case, visit every node in both trees.
+# Space Complexity: O(h) -> O(n) -> Store recursive calls on the stack up to height of tree. In worst case, h == n.
