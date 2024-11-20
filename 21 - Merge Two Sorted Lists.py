a9239fe90ca62fd705955fa5d74e4c814e75e603
Joshua <jhellst@gmail.com>
2024-11-20 10:30:38 -0800

    Added optimal solution to merge 2 linked lists in-order.

--- 21 - Merge Two Sorted Lists.py
+++ 21 - Merge Two Sorted Lists.py
@@ -56,3 +56,39 @@ class Solution:
 
 # Time Complexity: O(n + m) -> Visit every node in both linked lists once.
 # Space Complexity: O(1) -> No additional storage used.
+
+
+
+# 2nd Solution:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # 2 sorted lists -> merge them (in order) and return the head of the combined list.
+
+        tail = ListNode(0)
+        dummy = tail
+
+        while list1 and list2:
+            if list1.val <= list2.val: # list1 has lower val to add.
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        # Now, either both lists are empty or 1 list still contains unvisited nodes. Append the rest of this list (if it exists).
+        if list1:
+            tail.next = list1
+        if list2:
+            tail.next = list2
+
+        return dummy.next
+
+# Time Complexity: O(n + m) -> In worst case, visit every node in both lists.
+# Space Complexity: O(1) -> No additional storage used.
