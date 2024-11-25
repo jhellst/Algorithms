d1080ea77dab474fd6f329628a795799353c4719
Joshua <jhellst@gmail.com>
2024-11-25 11:59:23 -0800

    Added solution to find h-index from list of researcher's citations.

--- 274 - H-Index.py
+++ 274 - H-Index.py
@@ -33,3 +33,37 @@ class Solution:
                 return h_count
 
         return 0
+
+
+
+
+
+# 2nd Solution:
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        # Return the H-Index of the researcher, given an array of citations.
+        # Process:
+        #   - Sort citations array and traverse from highest to lowest, tracking count and value.
+        #   - Once count < cur_val, we can return the val (that is the H-Score).
+
+        citations.sort(reverse=True)
+
+        cur_val = 0
+        cur_count = 0
+        res = 0
+
+        for citation in citations:
+            cur_val = citation
+            cur_count += 1
+
+            if cur_count == cur_val:
+                return cur_val
+            elif cur_count < cur_val:
+                res = max(res, cur_count)
+
+        # Now, we know that cur_count was never <= cur_val. For that reason, we want to return cur_count.
+        return res
+
+# Time Complexity: O(n + n * log(n)) -> Sort array of length n, then traverse it once.
+# Space Complexity: O(1) -> No additional storage used.
