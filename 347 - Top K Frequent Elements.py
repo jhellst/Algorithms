b2d778687c852b91ecbc316f8064cdef7223c74f
Joshua <jhellst@gmail.com>
2025-02-26 13:11:11 -0800

    Added optimal solution to find top k frequent elements in an array. Solution uses heap and counter/dict.

--- 347 - Top K Frequent Elements.py
+++ 347 - Top K Frequent Elements.py
@@ -58,3 +58,26 @@ class Solution:
 
 # Time Complexity: O(n + n * log(k)) -> O(n * log(k)) -> Create a Counter, then traverse every key in the Counter and conduct heap operations on a heap of length k.
 # Space Complexity: O(n + k) -> Store n elements in a Counter, and store k elements on a heap.
+
+
+
+# 3rd Solution:
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Return the k most frequent elements.
+        # Process: Create a counter for each key, and then push each [count, value] onto a min_heap.
+        #   - Maintain min_heap size of k, and return ONLY the value from the remaining items on the heap.
+
+        min_heap = []
+        c = Counter(nums)
+        for val in c:
+            heapq.heappush(min_heap, [c[val], val])
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+
+        res = [val for count, val in min_heap]
+        return res
+
+# Time Complexity: O(n * log(k)) -> Create a counter from every value in nums, and push/pop from heap of max size k.
+# Space Complexity: O(n + k) -> Store up to n items in a counter/dict, and store up to k items in a heap.
